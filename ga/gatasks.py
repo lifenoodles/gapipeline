f0675b438628da83c627c63dc46a803eee98acbc
Donagh Hatton <donaghhatton@gmail.com>
2013-10-11 00:34:28 +0100

    split into task/gatask with mixin
    Signed-off-by: Donagh Hatton <donaghhatton@gmail.com>

--- ga/gatasks.py
+++ ga/gatasks.py
@@ -1,42 +1,34 @@
 import abc
-from parameter import Parameter
+from task import Task, Parameter, ParameterisedMixin, MemoryUnitsMixin
 
-
-class Operator(object):
+class GaTask(Task, ParameterisedMixin, MemoryUnitsMixin):
     __metaclass__ = abc.ABCMeta
 
-    @abc.abstractmethod
-    def execute(self, memory):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def parameter_list(self):
-        raise NotImplementedError
-
-
-class TestOperator(Operator):
+class Test(GaTask):
     def __init__(self):
         pass
 
-    def execute(self, memory):
+    def __call__(self, message):
         print "Test executed"
 
     def parameter_list(self):
         return []
 
 
-class TestOperator2(Operator):
+class Test2(GaTask):
     def __init__(self, param):
         self.param = param
 
-    def execute(self, memory):
+    def __call__(self, message):
         print "Test 2 executed: %s" % str(self.param)
 
     def parameter_list(self):
         return [Parameter(int, "Test Parameter")]
 
+    def requires(self):
+        return [True]
+
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-
