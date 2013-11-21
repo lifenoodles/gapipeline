bfe668473884409a6baebaf35e9864e4d07862e0
Donagh Hatton <donaghhatton@gmail.com>
2013-11-21 17:23:03 +0000

    general controller added

---
+++ tasks/controllers.py
@@ -0,0 +1,15 @@
+import pypline
+
+
+class GeneralController(pypline.Task):
+    epsilon = 10e-7
+
+    def __init__(self, generations):
+        self.generations = generations
+
+    def process(self, message, pipeline):
+        if message.best is not None \
+            and abs(message.best.fitness - message.target_fitness) \
+                < GeneralController.epsilon:
+            return True
+        return message.generation == self.generations
