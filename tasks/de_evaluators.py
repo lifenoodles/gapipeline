2843cead92c38cfa2d15b2429cff65df0de2149e
Donagh Hatton <donaghhatton@gmail.com>
2013-11-25 12:42:14 +0000

    dejon1 evaluator implementation

--- tasks/de_evaluators.py
+++ tasks/de_evaluators.py
@@ -4,4 +4,8 @@ import pypline
 class DeJongOneEvaluator(pypline.Task):
     def process(self, message, pipeline):
         for solution in message.population:
-            print solution
+            fitness = 0
+            for gene in solution.genes:
+                fitness += gene * gene
+            solution.fitness = fitness
+        return message
