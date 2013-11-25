ce217478b015d5c17a64405055afbbe2b3f36247
Donagh Hatton <donaghhatton@gmail.com>
2013-11-25 13:00:17 +0000

    evaluators deal with candidates now

--- tasks/de_evaluators.py
+++ tasks/de_evaluators.py
@@ -3,7 +3,7 @@ import pypline
 
 class DeJongOneEvaluator(pypline.Task):
     def process(self, message, pipeline):
-        for solution in message.population:
+        for solution in message.candidates:
             fitness = 0
             for gene in solution.genes:
                 fitness += gene * gene
