ebb319ba240c75d3bf2c45299befa0fbe9d58f27
Donagh Hatton <donaghhatton@gmail.com>
2013-11-25 13:04:48 +0000

    AllReplacer added

--- tasks/replacers.py
+++ tasks/replacers.py
@@ -9,3 +9,12 @@ class BetterReplacer(pypline.Task):
             if message.candidates[i].fitness < message.population[i].fitness:
                 message.population[i] = message.candidates[i]
         return message
+
+
+class AllReplacer(pypline.Task):
+    def process(self, message, pipeline):
+        assert(len(message.population) == len(message.candidates),
+               "Population length and candidate length are not equal!")
+        for i in xrange(len(message.population)):
+            message.population[i] = message.candidates[i]
+        return message
