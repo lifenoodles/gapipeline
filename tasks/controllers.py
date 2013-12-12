ffa7dbb70599ef4f52fc217abfad6cbf38e8d23d
Donagh Hatton <donaghhatton@gmail.com>
2013-12-12 13:42:41 +0000

    general controller changed to generation controller

--- tasks/controllers.py
+++ tasks/controllers.py
@@ -1,17 +1,19 @@
 import pypline
 
 
-class GeneralController(pypline.Task):
+class GenerationController(pypline.Task):
     epsilon = 10e-7
 
     def __init__(self, generations):
         self.generations = generations
 
     def process(self, message, pipeline):
+        assert message.generation <= self.generations, \
+            "%d is greater than cutoff of %d." % \
+            (message.generation, self.generations)
         if message.best is not None \
             and abs(message.best.fitness - message.target_fitness) \
-                < GeneralController.epsilon:
+                < GenerationController.epsilon:
             return True
-        terminate = message.generation == self.generations
         message.generation += 1
-        return terminate
+        return message.generation > self.generations
