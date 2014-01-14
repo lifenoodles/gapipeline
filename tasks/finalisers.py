160a958516e66a2ba162a6392f31baedea5c84fb
Donagh Hatton <donaghhatton@gmail.com>
2014-01-14 13:36:07 +0000

    intialisers and finalisers taken into account for description builder now

--- tasks/finalisers.py
+++ tasks/finalisers.py
@@ -5,8 +5,11 @@ import pypline
 class DescriptionBuilder(pypline.Task):
     def process(self, message, pipeline):
         description = {}
-        tasklist = pipeline._tasks
+        tasklist = pipeline._tasks + [pipeline._controller] \
+            + pipeline._initialisers + pipeline._finalisers
         for task in tasklist:
             if hasattr(task, "getDescription"):
                 description.update(getattr(task, "getDescription")())
+        message.description = description
         print description
+        return message
