dcfb474a1aecb3beeb38af87302766c5a02d8530
Donagh Hatton <donaghhatton@gmail.com>
2014-01-16 17:20:56 +0000

    Dejong2 and Rastrigin6 evaluation added

--- tasks/de_evaluators.py
+++ tasks/de_evaluators.py
@@ -1,4 +1,5 @@
 import pypline
+import math
 
 
 @pypline.requires("trials")
@@ -19,3 +20,45 @@ class DeJongOneEvaluator(pypline.Task):
 
     def getDescription(self):
         return { "problem": "Dejong1" }
+
+
+@pypline.requires("trials")
+@pypline.provides("best")
+class DeJongTwoEvaluator(pypline.Task):
+    """
+    DejongTwo function evaluator
+    """
+    def process(self, message, pipeline):
+        for solution in message.trials:
+            fitness = 0
+            genes = solution.genes
+            for i in xrange(len(solution.genes) - 1):
+                fitness += (100 * (genes[i] - genes[i + 1]**2)**2) + (1 - genes[i])**2
+            solution.fitness = fitness
+            if message.best is None or solution.fitness < message.best.fitness:
+                message.best = solution
+        return message
+
+    def getDescription(self):
+        return { "problem": "Dejong2" }
+
+
+@pypline.requires("trials")
+@pypline.provides("best")
+class RastriginSixEvaluator(pypline.Task):
+    """
+    RastriginSix function evaluator
+    """
+    def process(self, message, pipeline):
+        for solution in message.trials:
+            genes = solution.genes
+            fitness = 10 * len(genes)
+            for i in xrange(len(solution.genes)):
+                fitness += genes[i]**2 - (10 * math.cos(2 * math.pi * genes[i]))
+            solution.fitness = fitness
+            if message.best is None or solution.fitness < message.best.fitness:
+                message.best = solution
+        return message
+
+    def getDescription(self):
+        return { "problem": "RastriginSix" }
