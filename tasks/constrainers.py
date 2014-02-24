5ca64b03d316dd6da25fcb9b61b719bb1251c187
Donagh Hatton <donaghhatton@gmail.com>
2014-02-24 09:44:55 +0000

    bounds constrainer added

---
+++ tasks/constrainers.py
@@ -0,0 +1,13 @@
+import pypline
+
+
+@pypline.requires("trials")
+class BoundsConstrainer(pypline.Task):
+    def process(self, message, pipeline):
+        for trial in message.trials:
+            for i, gene in enumerate(trial.genes):
+                if gene < message.lower:
+                    trial.genes[i] = message.lower
+                elif gene > message.upper:
+                    trial.genes[i] = message.upper
+        return message
