7f01aa329eecc0ee22540bba3a64bb99e0706057
Donagh Hatton <donaghhatton@gmail.com>
2014-03-17 13:33:11 +0000

    PF visualiser added

--- tasks/visualisers.py
+++ tasks/visualisers.py
@@ -14,9 +14,16 @@ def rastrigin(x):
     return 10 + (x**2 - (10 * math.cos(2 * math.pi * x)))
 
 
+def fc(cs, z):
+    f = 0
+    for j, c in enumerate(cs):
+        f += c * (z ** j)
+    return f
+
+
 class RastriginSixVisualiser(pypline.Task):
     def __init__(self):
-        self._range = [x for x in float_range(-5.2, 5.2, 0.1)]
+        self._range = [x for x in float_range(-1.2, 1.2, 0.05)]
         self._values = [rastrigin(x) for x in self._range]
         self._last_fitness = []
         self._last_genes = []
@@ -24,32 +31,52 @@ class RastriginSixVisualiser(pypline.Task):
         self._last_colour = 1
         pyplot.ion()
     def process(self, message, pipline):
-        # pyplot.draw()
-        # self._figure.canvas.draw()
         genes = [f.genes[0] for f in message.trials]
         fitness = [f.fitness for f in message.trials]
         pyplot.gcf().clear()
         pyplot.plot(self._range, self._values)
-        # pyplot.plot(self._last_genes, self._last_fitness, \
-        #     self._colours[self._last_colour])
-        # new_colour = (self._last_colour + 1) % len(self._colours)
-        # self._last_colour = new_colour
-        # pyplot.plot(genes, fitness, self._colours[new_colour])
         pyplot.plot(genes, fitness, 'ro')
-        arrow_x = [x - y for x, y in zip(genes, self._last_genes)]
-        arrow_y = [x - y for x, y in zip(fitness, self._last_fitness)]
-        # pyplot.quiver(self._last_genes, self._last_fitness,
-        #         arrow_x, arrow_y, width=0.003)
-        # pyplot.quiver([0], [0], [5], [5], width=0.003, scale=1)
-        # if len(self._last_genes) == len(genes):
-        #     for i in xrange(len(genes)):
-        #         pyplot.arrow(self._last_genes[i], self._last_fitness[i],
-        #             arrow_x[i], arrow_y[i], width=0.003,
-        #             length_includes_head=True, shape="full", head_width=0.3)
         pyplot.pause(0.0001)
         time.sleep(0.5)
         self._last_genes = genes
         self._last_fitness = fitness
+        return message
+
+if __name__ == "__main__":
+    RastriginSixVisualiser().process(None, None)
+
+
+class PolynomialFittingVisualiser(pypline.Task):
+    def __init__(self):
+        self._range = [x for x in float_range(-5.2, 5.2, 0.1)]
+        self._last_best = None
+        # self._values = [rastrigin(x) for x in self._range]
+        self._last_fitness = []
+        self._last_genes = []
+        self._colours = ['ro', 'go']
+        self._last_colour = 1
+        pyplot.ion()
+    def process(self, message, pipline):
+        # genes = [f.genes[0] for f in message.trials]
+        # fitness = [f.fitness for f in message.trials]
+        if self._last_best == message.best:
+            return message
+        self._last_best = message.best
+        pyplot.gcf().clear()
+        pyplot.vlines([-1.2, 1.2], -1, 5)
+        pyplot.vlines([-1, 1], 1, 5)
+        pyplot.hlines(1, -1, 1)
+        pyplot.hlines(-1, -1.2, 1.2)
+        points = [fc(message.best.genes, x) for x in self._range]
+        pyplot.plot(self._range, points)
+        pyplot.ylim([-3, 3])
+        pyplot.xlim([-1.5, 1.5])
+        pyplot.axhline(0, color='black')
+        pyplot.axvline(0, color='black')
+        # pyplot.plot(genes, fitness, 'ro')
+        pyplot.pause(0.0001)
+        # self._last_genes = genes
+        # self._last_fitness = fitness
         return message
 
 if __name__ == "__main__":
