87b7de0c6744515c9597e6faf77d5a61c2dc9c6c
Donagh Hatton <donaghhatton@gmail.com>
2014-03-17 13:32:42 +0000

    polynomial fitting evaluator complete

--- tasks/de_evaluators.py
+++ tasks/de_evaluators.py
@@ -109,29 +109,39 @@ class RastriginSixEvaluator(pypline.Task):
 @pypline.provides("best")
 class PolynomialFittingEvaluator(pypline.Task):
     """
-    Polynomial fitting evaluator for clogj * z^j for j = (0..2k)
+    Polynomial fitting evaluator for cj * z^j for j = (0..2k)
     """
     def fc(self, cs, z):
         f = 0
         for j, c in enumerate(cs):
             f += c * (z ** j)
+        return f
 
-    def t2k(self, z):
-
+    def t2k(self, z, n):
+        memo = [1, z]
+        for i in xrange(2, n):
+            memo.append(2 * z * memo[i - 1] - memo[i - 2])
+        return memo[-1]
 
     def process(self, message, pipeline):
         for solution in message.trials:
             fitness = 0
-            for p in xrange(100):
+            for p in [-1, 1]:
+                fcpi = self.fc(solution.genes, p)
+                if -1 > fcpi or fcpi > 1:
+                    fitness += (1 - fcpi) ** 2
+            for p in xrange(150):
                 pi = random.random() * 2 - 1
                 # compute fc(pi)
                 fcpi = self.fc(solution.genes, pi)
                 if -1 > fcpi or fcpi > 1:
                     fitness += (1 - fcpi) ** 2
-            fcpi = fc(solution.genes, 1.2)
-
-
-            fcpi = fc(solution.genes, -1.2)
+            for p in [1.2, -1.2]:
+                fcpi = self.fc(solution.genes, p)
+                tkpi = self.t2k(p, len(solution.genes))
+                cond = fcpi - tkpi
+                if cond < 0:
+                    fitness += cond ** 2
 
             solution.fitness = fitness
             if message.best is None or solution.fitness < message.best.fitness:
@@ -139,4 +149,8 @@ class PolynomialFittingEvaluator(pypline.Task):
         return message
 
     def getDescription(self):
-        return { "problem": "RastriginSix" }
+        return { "problem": "Polynomial Fitting" }
+
+
+if __name__ == "__main__":
+    print PolynomialFittingEvaluator().t2k(1.2, 9)
