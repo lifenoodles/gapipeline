d3d694e40a213f311d2a33bfb6a21668b225f9a3
Donagh Hatton <donaghhatton@gmail.com>
2014-07-21 12:17:40 +0100

    added verbose flag to runner

--- runner.py
+++ runner.py
@@ -4,8 +4,11 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="GA Pipeline Runner")
+    parser.add_argument("-v", action="store_const", const=True,
+        help="Verbose mode")
     parser.add_argument("filename")
     args = parser.parse_args()
+    print dir(args)
     filename = args.filename
     filetype = os.path.splitext(filename)[1]
     if filetype == ".yaml":
@@ -15,6 +18,6 @@ if __name__ == "__main__":
         manager.generate_pipelines()
         print "Generated %i pipeline%s" % (len(manager.pipelines),
             "s" if len(manager.pipelines) > 1 else "")
-        manager.execute(verbose=True)
+        manager.execute(verbose=args.v)
 
     print "Run Complete"
