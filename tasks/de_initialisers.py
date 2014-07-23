9ac0b80e5ff8f3b9156fd6b31e534c96e9513414
Donagh Hatton <donaghhatton@gmail.com>
2014-07-23 17:19:58 +0100

    spacing

--- tasks/de_initialisers.py
+++ tasks/de_initialisers.py
@@ -101,16 +101,19 @@ class RastriginSixInitialiser(DeInitialiser):
         super(RastriginSixInitialiser, self).__init__(pop_size, solution_size)
 
     def process(self, message, pipeline):
-        de = initialise_random_de(self.pop_size, self.solution_size, -5.12, 5.12)
+        de = initialise_random_de(
+            self.pop_size, self.solution_size, -5.12, 5.12)
         de_evaluators.RastriginSixEvaluator().process(de, None)
         return de
 
 
 class PolynomialFittingInitialiser(DeInitialiser):
     def __init__(self, pop_size, solution_size):
-        super(PolynomialFittingInitialiser, self).__init__(pop_size, solution_size)
+        super(PolynomialFittingInitialiser, self).__init__(
+            pop_size, solution_size)
 
     def process(self, message, pipeline):
-        de = initialise_random_de(self.pop_size, self.solution_size, -5.12, 5.12)
+        de = initialise_random_de(
+            self.pop_size, self.solution_size, -5.12, 5.12)
         de_evaluators.PolynomialFittingEvaluator().process(de, None)
         return de
